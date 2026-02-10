514bd5160a4fd295506602d576359674cfe4d889
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2026-02-11 03:12:19 +0800

    feat(employees.serializers): made object creation atomic and added resend
    - resend will be the email provider service that will be used to send
    the credentials of the created employee

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,7 +1,9 @@
+from django.db import transaction
 from django.utils.crypto import get_random_string
 from rest_framework import serializers
 
 from employees.models import Employee
+from shared.utils.email import send_resend_email
 from users.models import CustomUser
 from users.serializers import CustomUserSerializer
 
@@ -23,9 +25,29 @@ class EmployeeSerializer(serializers.ModelSerializer[Employee]):
         if user_data["role"] != CustomUser.Role.EMPLOYEE:
             raise serializers.ValidationError({"role": "Role must be employee."})
 
+        try:
+            # We use an atomic transaction to make sure that all operations are successfull
+            # for the objects to be created
+            with transaction.atomic():
                 user = CustomUser.objects.create_user(**user_data)
+                employee = Employee.objects.create(user=user, **validated_data)
 
-        employee = Employee.objects.create(user=user, **validated_data)
+                send_resend_email(
+                    subject="Your Account Credentials",
+                    recipient=user.email,
+                    html=f"""
+                    <p>Hello,</p>
+                    <p>Your account has been created. Here are your login credentials:</p>
+                    <p>Email: <strong>{user.email}</strong></p>
+                    <p>Temporary Password: <strong>{original_password}</strong></p>
+                    <p>Please change your password upon first login.</p>
+                    """,
+                )
+        except Exception as error:
+            raise serializers.ValidationError(
+                {"error": f"Failed to create account or send email: {str(error)}"}
+            )
+
         return employee
 
     def update(self, instance: Employee, validated_data):
